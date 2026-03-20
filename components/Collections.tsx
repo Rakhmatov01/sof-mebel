e4cb00b6f0be826f9cd62f0bb689cc965978e5bd
Lochinbek <lochinbekrahmatov6@gmail.com>
2026-03-21 00:37:39 +0500

    finished first task

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -14,6 +14,7 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1758448755778-90ebf4d0f1e7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBjdXNoaW9uZWQlMjBzb2ZhJTIwYmVpZ2V8ZW58MXx8fHwxNzcxODUwNDcwfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Sumptuous velvet upholstery with cloud-like comfort",
+    imageRatio:"5 / 3"
   },
   {
     id: 2,
@@ -24,6 +25,7 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1765663241884-ebd171bdda1d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxkZXNpZ25lciUyMHZlbHZldCUyMGFybWNoYWlyfGVufDF8fHx8MTc3MTg1MDQ3MHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Statement pieces with sculptural elegance",
+    imageRatio:"5 / 3"
   },
   {
     id: 3,
@@ -34,6 +36,7 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1759722665610-e13e59aa117b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtb2Rlcm4lMjBzZWN0aW9uYWwlMjBjb3VjaHxlbnwxfHx8fDE3NzE4NTA0NzF8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Customizable configurations for modern living",
+    imageRatio:"5 / 4"
   },
   {
     id: 4,
@@ -44,6 +47,7 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1767050321604-a2654be8fad0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcmVtaXVtJTIwbG91bmdlJTIwY2hhaXIlMjBjdXNoaW9ufGVufDF8fHx8MTc3MTg1MDQ3MXww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Perfect balance of form and relaxation",
+    imageRatio:"5 / 4"
   },
   {
     id: 5,
@@ -54,6 +58,7 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1768573264026-b540abdc3384?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxlbGVnYW50JTIwY2hhaXNlJTIwbG9uZ3VlfGVufDF8fHx8MTc3MTg1MDQ3MXww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Timeless elegance meets contemporary design",
+    imageRatio:"5 / 4"
   },
   {
     id: 6,
@@ -64,6 +69,28 @@ const collections = [
     image:
       "https://images.unsplash.com/photo-1759264244744-4b0077534bfd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBhY2NlbnQlMjBjaGFpciUyMGludGVyaW9yfGVufDF8fHx8MTc3MTg1MDQ3MXww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
     description: "Bold designs that command attention",
+    imageRatio:"5 / 4"
+  }, {
+    id: 7,
+    name: "Velvet Luxe",
+    category: "Divanlar va Burchakli Divanlar",
+    price: "From $3,299",
+    rating: 4.9,
+    image:
+      "https://images.unsplash.com/photo-1758448755778-90ebf4d0f1e7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxsdXh1cnklMjBjdXNoaW9uZWQlMjBzb2ZhJTIwYmVpZ2V8ZW58MXx8fHwxNzcxODUwNDcwfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    description: "Sumptuous velvet upholstery with cloud-like comfort",
+    imageRatio:"5 / 3"
+  },
+  {
+    id: 8,
+    name: "Aurora Collection",
+    category: "Kreslolar",
+    price: "From $1,899",
+    rating: 4.8,
+    image:
+      "https://images.unsplash.com/photo-1765663241884-ebd171bdda1d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxkZXNpZ25lciUyMHZlbHZldCUyMGFybWNoYWlyfGVufDF8fHx8MTc3MTg1MDQ3MHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    description: "Statement pieces with sculptural elegance",
+    imageRatio:"5 / 3"
   },
 ];
 
@@ -76,13 +103,7 @@ const categories = [
 ];
 
 export function Collections() {
-  const [activeCategory, setActiveCategory] = useState("Barchasi");
   const [hoveredId, setHoveredId] = useState<number | null>(null);
-
-  const filteredCollections =
-    activeCategory === "Barchasi"
-      ? collections
-      : collections.filter(item => item.category === activeCategory);
 
   return (
     <div className='relative w-full py-24 lg:py-32'>
@@ -95,69 +116,23 @@ export function Collections() {
         />
       </div>
 
-      <div className='relative z-10 max-w-[1400px] mx-auto px-6 lg:px-12'>
-        <div className='text-center mb-16 lg:mb-20'>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6 }}
-            className='inline-block px-4 py-2 rounded-full mb-6 bg-greenDeep/5 border border-greenDeep/10'
-          >
-            <span className='text-sm tracking-wide text-greenDeep'>
-              Maxsus saralangan to‘plam
-            </span>
-          </motion.div>
+      <div className='relative z-10 max-w-[1400px] mx-auto px-6 lg:px-16'>
+<div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+  <motion.div
+    initial={{ opacity: 0, y: 30 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    transition={{ duration: 0.6, delay: 0.2 }}
+    className="w-80 sm:w-96 p-4 px-8 sm:px-16 rounded-full bg-white "
+  >
+    <h1 className="text-2xl text-goldAccent font-extrabold text-center">SOF-MEBEL</h1>
+    <h2 className="text-lg tracking-wider text-center">Yumshoq mebellarning</h2>
+    <h2 className="text-md font-semibold tracking-wider text-center">Sara dizaynli kolleksiyalari</h2>
+  </motion.div>
+</div>
 
-          <motion.h2
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.1 }}
-            className='text-4xl lg:text-5xl xl:text-6xl mb-6 tracking-tight font-serif text-greenDeep font-light -tracking-[0.02em]'
-          >
-            Yumshoq mebellarning
-            <span className='block mt-2 text-goldAccent font-normal'>
-              Sara dizaynli kolleksiyalari
-            </span>
-          </motion.h2>
-
-          <motion.p
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-            className='text-lg max-w-2xl mx-auto text-greenDeep/60'
-          >
-            Hayotdagi nozik va sifatni qadrlaydiganlar uchun yaratilgan, yuqori
-            sifatli yumshoq mebellarning saralangan to‘plamini kashf eting.
-          </motion.p>
-        </div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6, delay: 0.3 }}
-          className='flex flex-wrap justify-center gap-3 mb-16'
-        >
-          {categories.map(category => (
-            <button
-              key={category}
-              onClick={() => setActiveCategory(category)}
-              className={`px-6 py-3 rounded-full transition-all duration-300 border font-normal ${
-                activeCategory === category
-                  ? "bg-goldAccent text-greenDeep border-goldAccent font-medium"
-                  : "bg-greenDeep/5 text-greenDeep/60 border-greenDeep/10"
-              }`}
-            >
-              {category}
-            </button>
-          ))}
-        </motion.div>
-
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-10'>
-          {filteredCollections.map((item, index) => (
+        <div className='columns-2 md:columns-3 lg:columns-4 gap-2 sm:gap-4 lg:gap-6'>
+          {collections.map((item, index) => (
             <motion.div
               key={item.id}
               initial={{ opacity: 0, y: 30 }}
@@ -168,7 +143,8 @@ export function Collections() {
               onMouseLeave={() => setHoveredId(null)}
               className='group relative'
             >
-              <div className='relative h-[400px] rounded-3xl overflow-hidden mb-6'>
+              <div className='relative rounded-3xl overflow-hidden mb-6'
+              style={{aspectRatio : item.imageRatio}}>
                 <motion.div
                   animate={{
                     scale: hoveredId === item.id ? 1.05 : 1,
@@ -210,7 +186,7 @@ export function Collections() {
                 </motion.button>
               </div>
 
-              <div className='space-y-3'>
+              {/* <div className='space-y-3'>
                 <div className='flex items-start justify-between gap-4'>
                   <div>
                     <h3 className='text-2xl mb-1 text-greenDeep font-normal'>
@@ -237,22 +213,10 @@ export function Collections() {
                   </span>
                   <ArrowRight className='w-4 h-4 text-goldAccent transition-transform group-hover/link:translate-x-1' />
                 </motion.button>
-              </div>
+              </div> */}
             </motion.div>
           ))}
         </div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-          className='text-center mt-16'
-        >
-          <button className='px-10 py-4 rounded-full transition-all duration-300 hover:shadow-xl border-2 border-greenDeep text-greenDeep font-medium'>
-            Barcha kolleksiyalar
-          </button>
-        </motion.div>
       </div>
     </div>
   );
